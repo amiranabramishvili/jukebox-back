9850972b0e0bbd7b8ea7155279ada2c6b81595db
amiranabramishvili <amiran.abramishvili.93@gmail.com>
2025-06-16 19:04:33 +0400

    feat/GENZ-10/created author 90%

---
+++ src/author/dto/create-author.dto.ts
@@ -0,0 +1,14 @@
+import {IsString } from "class-validator";
+
+export class CreateAuthorDto {
+    @IsString()
+    firstName: string;
+
+    @IsString()
+    lastname : string;
+
+    @IsString()
+    biography: string;
+
+    
+}
