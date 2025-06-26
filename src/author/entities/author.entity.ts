129f060dc0658036f42db4acef447babc5059235
amiranabramishvili <amiran.abramishvili.93@gmail.com>
2025-06-26 21:22:33 +0400

    done

--- src/author/entities/author.entity.ts
+++ src/author/entities/author.entity.ts
@@ -1,1 +1,30 @@
-export class Author {}
+import { CreateMusicDto } from "src/music/dto/create-music.dto";
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Author {
+    @PrimaryGeneratedColumn()
+    id: number
+    
+    @Column({type: 'varchar'})
+    firstName: string
+
+    @Column({type: 'varchar'})
+    lastName: string
+
+    @Column({type: 'text'})
+    biography: string
+
+    @Column({type: 'json'})
+    music: CreateMusicDto
+
+    @CreateDateColumn()
+    createdAt: Date
+
+    @UpdateDateColumn()
+    updatedAt: Date
+
+    @DeleteDateColumn()
+    deleteAt: Date
+
+}
