b4f24cf61a10373354d1cf7f0618343546f46aab
amiranabramishvili <amiran.abramishvili.93@gmail.com>
2025-06-27 17:47:30 +0400

    reset app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthorModule } from './author/author.module';
 import { AlbumsModule } from './albums/albums.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { MusicModule } from './music/music.module';
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'Novatori131!',
-      database: 'jukebox',
-      autoLoadEntities: true,
-      synchronize: true
-    }),
     MusicModule,
     AuthorModule,
     AlbumsModule],
