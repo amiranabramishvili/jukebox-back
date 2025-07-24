145ebf86719d233fd4e8ef32e2b838180229710c
amiranabramishvili <amiran.abramishvili.93@gmail.com>
2025-07-24 15:02:41 +0400

    Update file.entity.ts

--- src/files/entities/file.entity.ts
+++ src/files/entities/file.entity.ts
@@ -1,9 +1,8 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity } from "src/base/base.entity";
+import {Column, Entity } from "typeorm";
 
 @Entity()
-export class FileEntity {
-    @PrimaryGeneratedColumn()
-    id: number;
+export class FileEntity extends BaseEntity {
 
     @Column()
     fileName: string;
